04439ee713b383117ff616c87a96ef9d42f40475
Hans Czka Maya N. Abapo <20200019568@my.xu.edu.ph>
2023-05-05 08:22:24 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,78 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lexend:100,200,300,400,500,600,700,800,900">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sora:100,200,300,400,500,600,700,800,900">
+    
+  <div class="container">
+    <div class="content">  
+      <SiteHeader/>
+      <HeroCover/>
+      <HistoryBox/>
+      <div class="spacer">
+        <HistoryText/>
+      </div>
+    </div>
+
+    <footer class ="footer">
+        <FooterFeet/>
+    </footer>
+  </div>
+
 </template>
 
+<script>
+import SiteHeader from './components/SiteHeader.vue'
+import HeroCover from './components/HeroCover.vue'
+import HistoryBox from './components/HistoryBox.vue'
+import HistoryText from './components/HistoryText.vue'
+import FooterFeet from './components/FooterFeet.vue'
+export default {
+  name: 'App',
+  components: {
+    SiteHeader,
+    HeroCover,
+    HistoryBox,
+    HistoryText,
+    FooterFeet
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+:root{
+  --white: #FFFFFF;
+  --prime: #2B3B73;
+  --light: #9EABD7;
+  --black: #1F1D1D;
+  --acc: #B99433;
+  --dacc: #97762f;
+  --header: "Sora", "Verdana", sans-serif;
+  --text: "Lexend", "Arial", sans-serif;
+}
+html, body{
+  margin: 0;
+  height: 100%;
+} #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--black);
+  margin: 0px;
+  text-align: left;
 }
-
-nav {
-  padding: 30px;
+.spacer{
+  margin-top: 40px;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container{
+  position: relative;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.content{
+  flex: 1;
 }
-</style>
+.footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+} </style>
