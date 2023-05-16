e78eec9f715f6df07c0f919c0148a9e0e5dc0e66
Zenia <2013001116@my.xu.edu.ph>
2023-05-16 13:25:53 +0000

    fix main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 main().catch(err => console.log(err));
 
 async function main() {
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/');` if your database has auth enabled
 }
 
 createApp(App).use(router).mount('#app')
