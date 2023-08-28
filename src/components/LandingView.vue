d2ab79df2faf63822e61fc168fe3e203e97d8899
JosiahJoseDeysolong <20200020446@my.xu.edu.ph>
2023-08-28 18:44:50 +0800

    Navbar and landing page added

---
+++ src/components/LandingView.vue
@@ -0,0 +1,73 @@
+<template>
+    <br>
+    <br>
+    <div class="container-fluid my-5 grey-b1">
+        <div class="row my-0 grey-b2">
+            <br>
+        </div>
+
+        <div class="container">
+        <div class="row align-items-center">
+
+            <div class="col-7 my-5">
+                 
+            <h1 class="text-center py-4 fw-bold mt-3 maroon-a1 work-f-size">
+                COME WORK WITH US
+            </h1>
+
+            
+            <h3 class="text-center py-3 maroon-a2 work2-f-size">
+                We provide simple solutions to complex problems
+            </h3>
+            <p class="text-center work3-f-size">
+                Apollo Technologies is an Information and Communication Technology Servicesorganisation 
+                that leads a group of companies engaged in the delivery of Software Services, IT and Network Infrastructure, and Services.
+            </p>
+            </div>
+
+            <div class="col-5 my-3">
+  
+                <div id="apollo-carousel" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active c-item">
+                        <img src="https://picsum.photos/id/7/800/600" class="d-block w-100 c-img" alt="...">
+                        </div>
+                        <div class="carousel-item c-item">
+                        <img src="https://picsum.photos/id/8/800/600" class="d-block w-100 c-img" alt="...">
+                        </div>
+                        <div class="carousel-item c-item">
+                        <img src="https://picsum.photos/id/6/800/600" class="d-block w-100 c-img" alt="...">
+                        </div>
+                    </div>
+
+                    <button class="carousel-control-prev" type="button" data-bs-target="#apollo-carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#apollo-carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-0 grey-b2">
+            <br>
+        </div>
+</div>
+</template>
+     
+<style>
+.c-item {
+    height: 480px;
+}
+
+.c-img {
+    height: 100%;
+    object-fit: cover;
+    filter: brightness(0.8)
+}
+</style>
---
+++ src/components/NavbarView.vue
@@ -0,0 +1,102 @@
+<template>
+
+    <div class="container-fluid maroon-b1">
+        <div class="container">
+            <div class="row">
+            <nav class="navbar py-0 fixed-top navbar-expand-md maroon-b1" 
+            id="navbar-apollo">
+                <img src="../assets/apollotech-logo-mobile.png" class="img-responsive d-block d-sm-none">
+                <img src="../assets/apollotech-logo.png" class="img-responsive d-none d-lg-block d-sm-block">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbar-drop-menu" aria-controls="navbar-drop-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" 
+                id="navbar-drop-menu">  
+                    <ul class="nav">
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#about-view">About Us</a>
+                        </li>
+                        
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#services-view">Industries</a>
+                        </li>
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#projects-view">Projects</a>
+                        </li>
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#partners-view">Partners</a>
+                        </li>
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#careers-view">Careers</a>
+                        </li>
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#location-view">Location</a>
+                        </li>
+                        <li class="nav-item px-2">
+                            <a class="btn btn-outline-light" href="#contact-view">Contact</a>
+                        </li>
+                     </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    </div>    
+
+
+ </template>
+
+<style>
+
+    .work-f-size{
+        font-size: 64px;
+    }
+
+    .work2-f-size{
+        font-size: 48px;
+    }
+
+    .work3-f-size{
+        font-size: 24px;
+    }
+
+
+    .maroon-a1{
+        color: #A70C0C;
+    }
+
+    .maroon-b1{
+        background-color: #A70C0C;
+    }
+
+    .maroon-a2{
+        color: #A60C0C;
+    }
+
+    .white-a1{
+        color: #FFFFFF;
+    }
+
+    .black-a1{
+        color: #000000;
+    }
+
+
+    .grey-a1{
+        color: #E9ECEF;
+    }
+
+    .grey-b1{
+        background-color: #E9ECEF;
+    }
+
+    .grey-a2{
+        color: #737573;
+    }
+
+    .grey-b2{
+        background-color: #737573;
+    }
+</style>
