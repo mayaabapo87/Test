046e9a33386e13a183bb4e32a7861821487a3067
Maya Abapo <mayaabapo87@gmail.com>
2023-09-18 17:02:12 +0800

    Added backend server for projects

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const { Pool } = require('pg');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// PostgreSQL database configuration
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'apollo',
   password: 'qwerty12731.',
-  port: 5432, // PostgreSQL default port
+  port: 5432, 
 });
 
 app.use(express.json());
 
-// Add the cors middleware here
-app.use(cors()); // This allows all CORS requests. You can configure it for specific origins if needed.
 
-// Define a route to fetch project data from the database
+app.use(cors()); 
+
+
 app.get('/projects', async (req, res) => {
   try {
     const { rows } = await pool.query('SELECT * FROM projects');
