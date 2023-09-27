022112b7a2400b5f181c226be93ed2dd5d1a9215
Maya Abapo <mayaabapo87@gmail.com>
2023-09-27 11:35:40 +0800

    Modified backend/Added sequelize as orm for database

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const path = require('path');
 
-app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.json());
 
 const projectsRoutes = require('./routes/projects');
 const servicesRoutes = require('./routes/services');
